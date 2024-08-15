fc967c5775eee3b9a0f91aaa2f6cec5e202a9697
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-08-15 10:04:40 +0600

    format

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,4 +1,4 @@
-import {gql, useQuery} from '@apollo/client'
+import { gql, useQuery } from '@apollo/client';
 
 const GET_USERS = gql`
   query GetUsers {
@@ -10,10 +10,10 @@ const GET_USERS = gql`
       phoneNumber
     }
   }
-`
+`;
 
 const Users = () => {
-  const {loading, error, data} = useQuery(GET_USERS);
+  const { loading, error, data } = useQuery(GET_USERS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -23,27 +23,47 @@ const Users = () => {
       <h1>USERS</h1>
       {!loading && !error && (
         <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-      <thead>
-        <tr>
-          <th style={{ border: '1px solid black', padding: '8px' }}>First Name</th>
-          <th style={{ border: '1px solid black', padding: '8px' }}>Last Name</th>
-          <th style={{ border: '1px solid black', padding: '8px' }}>Email</th>
-          <th style={{ border: '1px solid black', padding: '8px' }}>Phone No.</th>
-          <th style={{ border: '1px solid black', padding: '8px' }}>Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.users.map((user, index) => (
-          <tr key={index}>
-            <td style={{ border: '1px solid black', padding: '8px' }}>{user.firstName}</td>
-            <td style={{ border: '1px solid black', padding: '8px' }}>{user.lastName}</td>
-            <td style={{ border: '1px solid black', padding: '8px' }}>{user.email}</td>
-            <td style={{ border: '1px solid black', padding: '8px' }}>{user.phoneNumber}</td>
-            <td style={{ border: '1px solid black', padding: '8px' }}>{user.address}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid black', padding: '8px' }}>
+                First Name
+              </th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>
+                Last Name
+              </th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>
+                Email
+              </th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>
+                Phone No.
+              </th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>
+                Address
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.users.map((user, index) => (
+              <tr key={index}>
+                <td style={{ border: '1px solid black', padding: '8px' }}>
+                  {user.firstName}
+                </td>
+                <td style={{ border: '1px solid black', padding: '8px' }}>
+                  {user.lastName}
+                </td>
+                <td style={{ border: '1px solid black', padding: '8px' }}>
+                  {user.email}
+                </td>
+                <td style={{ border: '1px solid black', padding: '8px' }}>
+                  {user.phoneNumber}
+                </td>
+                <td style={{ border: '1px solid black', padding: '8px' }}>
+                  {user.address}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
