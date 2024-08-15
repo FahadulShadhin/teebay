57585d26b7fdf1603b28dca2b3dd312564162e82
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-08-15 13:37:27 +0600

    dockerize frontend and add example queries

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,71 +1,35 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_USERS = gql`
-  query GetUsers {
-    users {
-      firstName
-      lastName
-      email
-      address
-      phoneNumber
-    }
-  }
-`;
+import { useMutation, useQuery } from '@apollo/client';
+import { LOGIN } from '../gql/mutations/userMutations';
+import { GET_USERS } from '../gql/queries/userQueries';
+import { GET_PRODUCTS } from '../gql/queries/productQueries';
 
 const Users = () => {
-  const { loading, error, data } = useQuery(GET_USERS);
+  // const { loading, error, data } = useQuery(GET_USERS);
+  // const { loading, error, data } = useQuery(GET_PRODUCTS);
+  const [login, { loading, error, data }] = useMutation(LOGIN);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+  if (data) console.log(data.users);
+
+  const handleLogin = async () => {
+    try {
+      const response = await login({
+        variables: {
+          email: 'test@email.com',
+          password: '123',
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div>
-      <h1>USERS</h1>
-      {!loading && !error && (
-        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-          <thead>
-            <tr>
-              <th style={{ border: '1px solid black', padding: '8px' }}>
-                First Name
-              </th>
-              <th style={{ border: '1px solid black', padding: '8px' }}>
-                Last Name
-              </th>
-              <th style={{ border: '1px solid black', padding: '8px' }}>
-                Email
-              </th>
-              <th style={{ border: '1px solid black', padding: '8px' }}>
-                Phone No.
-              </th>
-              <th style={{ border: '1px solid black', padding: '8px' }}>
-                Address
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.users.map((user, index) => (
-              <tr key={index}>
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  {user.firstName}
-                </td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  {user.lastName}
-                </td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  {user.email}
-                </td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  {user.phoneNumber}
-                </td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>
-                  {user.address}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    <button onClick={handleLogin} disabled={loading}>
+      Login
+    </button>
   );
 };
 
